d64a21178788c9e1a337276ee0934ca76d1243db
Bayron_Chavarria <bayronchava@gmail.com>
2024-12-20 17:24:43 -0500

    Incorporada la funcionalidad de cambio de posición al slider.

--- js/slider.js
+++ js/slider.js
@@ -11,4 +11,17 @@
     buttonBefore.addEventListener('click', ()=>{
         changePosition(-1);
     });
+
+    const changePosition = (add)=>{
+        const currentProduct = document.querySelector('.product__body--show').dataset.id;
+        value = Number(currentProduct);
+        value+= add;
+    
+        sliders[Number(currentProduct)-1].classList.remove('product__body--show');
+        if(value === sliders.length+1 || value === 0){
+            value = value === 0 ? sliders.length  : 1;
+        }
+    
+        sliders[value-1].classList.add('product__body--show');
+    }
 })();
