796525c4667e1dda464dddffcc2dcda611dc11d9
Bayron_Chavarria <bayronchava@gmail.com>
2024-12-20 17:13:03 -0500

    Implementada la estructura base para la funcionalidad del slider.

---
+++ js/slider.js
@@ -0,0 +1,6 @@
+(function(){    
+    const sliders = [...document.querySelectorAll('.product__body')];
+    const buttonNext = document.querySelector('#next');
+    const buttonBefore = document.querySelector('#before');
+    let value;
+})();
